22a2a035d086f9b97a521bc38f0c2b9ffbf0c1e5
nufluxx <141886360+nufluxx@users.noreply.github.com>
2024-01-30 18:02:36 -0500

    cleanup

--- test.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("mousemove", (event) = > {
-    const cursorHighlight = document.querySelector(".cursor-highlight");
-    cursorHighlight.style.left = event.clientX + "px";
-    cursorHighlight.style.top = event.clientY + "px";
-});
-
